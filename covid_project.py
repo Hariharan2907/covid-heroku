24faa38edb65e28585c41b398ec707a1a294230c
Hariharan2907 <56897205+Hariharan2907@users.noreply.github.com>
2020-08-26 12:16:17 -0500

    Update to Code

--- covid_project.py
+++ covid_project.py
@@ -1,60 +1,130 @@
 import pandas as pd
 import streamlit as st
-
 import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import calendar 
+import datetime as dt
 
-from matplotlib.dates import DateFormatter
-import matplotlib.ticker as ticker
-from pylab import *
+#from datetime import datetime
+#from matplotlib.dates import DateFormatter
+#import matplotlib.ticker as ticker
+#from pylab import *
 
+st.title("All Cause and COVID-19 Deaths for 2019-2020")
+st.markdown("---")
+#CDC dataset
 url="https://data.cdc.gov/api/views/muzy-jte6/rows.csv?accessType=DOWNLOAD"
 #df=pd.read_csv(url,error_bad_lines=False)
 def load_data():
     df_filter=pd.read_csv(url,error_bad_lines=False, usecols=["Jurisdiction of Occurrence","MMWR Year","MMWR Week","Week Ending Date","All Cause","COVID-19 (U071, Multiple Cause of Death)","COVID-19 (U071, Underlying Cause of Death)"])
     return df_filter
-df_filter = load_data()
+#map
 
-#gk = df_filter.groupby('Jurisdiction of Occurrence')
-#list1 = gk.groups
 
-states = st.sidebar.multiselect("Choose states",df_filter['Jurisdiction of Occurrence'].unique())
+#population dataset
+pop = pd.read_csv("Population.csv")
+for col in pop.columns: 
+    print(col) 
 
+try:
+    df_filter = load_data()
+except urllib.error.URLError as e:
+    st.error(
+        """
+        **This demo requires internet access.**
+
+        Connection error: %s
+    """
+        % e.reason
+    )
+
+merged = pd.merge(df_filter,pop,left_on='Jurisdiction of Occurrence', right_on = 'States')
+st.write(merged)
+states = st.sidebar.multiselect("Choose states",merged['Jurisdiction of Occurrence'].unique())
+
+if not states:
+    st.error("Please select a state")
     
-st.write("Weekly Death Rate")
-new_df = df_filter[df_filter['Jurisdiction of Occurrence'].isin(states)]
+    
+st.subheader("Weekly Death Rate")
+new_df = merged[merged['Jurisdiction of Occurrence'].isin(states)]
+new_df['Population'] = new_df['Population'].str.replace(',','').astype(float)
+new_df['Capita'] = (new_df['All Cause']/new_df['Population']) * 1000
+new_df['Capita_COVID'] = (new_df['COVID-19 (U071, Underlying Cause of Death)']/new_df['Population']) * 1000
 st.write(new_df)
 df2 = new_df
 
+
 #plot 1
-new_df = new_df.drop(['All Cause','COVID-19 (U071, Multiple Cause of Death)'],axis=1)
-#new_df['month'] = pd.DatetimeIndex(new_df['Week Ending Date']).month
-#new_df['month'] = new_df['month'].apply(lambda x: calendar.month_abbr[x])
-plt.style.use('fivethirtyeight')
-plt.grid(color='#d4d4d4')
-#fig, ax = plt.subplots(figsize=(10,6))
-new_df=new_df.groupby(['MMWR Week','MMWR Year']).mean().unstack()
+new_df = new_df.drop(['All Cause','COVID-19 (U071, Multiple Cause of Death)','Jurisdiction of Occurrence','States'],axis=1)
+fig, ax = plt.subplots()
+plt.figure(figsize=(13,7))
 st.write(new_df)
-plot = new_df.plot.bar(figsize = (12,9))
-plot.legend(loc = 'upper right')
-plot.set_title("COVID-19 (Underlying Cause of Death) Deaths")
-plot.set_xlabel("Week Number")
-plot.set_ylabel("Number of Deaths")
-plot.yaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
-
-
+plt.title("COVID-19 Deaths", fontsize = 20, style = 'normal')
+sns.lineplot('MMWR Week','Capita_COVID',hue='MMWR Year',data=new_df,ci=None,palette = ['red','black'],legend = 'full')
+axes1 = plt.gca()
+axes1.set_xlabel("Week Number")
+plt.ylabel("Capita per 1000")
+x= new_df['MMWR Week']
+plt.xticks(np.arange(min(x), max(x)+1, 1.0))
+plt.xticks(rotation=90)
+plt.grid(linewidth=0.5)
 st.pyplot()
 
-#plot 2
-df2 = df2.drop(['COVID-19 (U071, Multiple Cause of Death)','COVID-19 (U071, Underlying Cause of Death)'],axis = 1)
-#fig, ax = plt.subplots(figsize=(10,6))
-#df2['month'] = pd.DatetimeIndex(df2['Week Ending Date']).month
-df2=df2.groupby(['MMWR Week','MMWR Year']).mean().unstack()
+    
 
-plot1 = df2.plot.bar(figsize = (12,9))
-plot1.legend()
-plot1.set_title("All Cause Deaths")
-plot1.set_xlabel("Week Number")
-plot1.set_ylabel("Number of Deaths")
+#new_df['month'] = pd.DatetimeIndex(new_df['Week Ending Date']).month
+#new_df['month'] = new_df['month'].apply(lambda x: calendar.month_abbr[x])
+#plt.style.use('fivethirtyeight')
+#plt.grid(color='#d4d4d4')
+#fig, ax = plt.subplots(figsize=(10,6))
+#new_df=new_df.groupby(['MMWR Week','MMWR Year']).mean().unstack()
+#st.write(new_df)
+#plot = new_df.plot.bar(figsize = (12,9))
+#plot.legend(loc = 'upper right')
+#plot.set_title("COVID-19 (Underlying Cause of Death) Deaths") 
+#plot.set_xlabel("Week Number")
+#plot.set_ylabel("Number of Deaths")
+#plot.yaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
+
+
+#st.pyplot()
+
+
+
+#plot 2
+
+df2 = df2.drop(['COVID-19 (U071, Multiple Cause of Death)','COVID-19 (U071, Underlying Cause of Death)','Population','All Cause','Jurisdiction of Occurrence','States'],axis = 1)
+fig, ax = plt.subplots()
+df2['month'] = pd.DatetimeIndex(df2['Week Ending Date']).month
+df2['month_name'] = df2['month'].apply(lambda x: calendar.month_abbr[x])
+df2['date'] = pd.DatetimeIndex(df2['Week Ending Date']).date
+df2['date'] = pd.to_datetime(df2['date'],format = '%Y-%m-%d')
+df2['MM-DD'] = df2['date'].dt.strftime('%m-%d')
+
+#df2=df2.groupby(['MMWR Week','MMWR Year']).mean().unstack()
+st.write(df2)
+plt.figure(figsize=(13,7))
+plt.title("All Cause Deaths", fontsize = 20, style = 'normal')
+sns.lineplot('MMWR Week','Capita',hue='MMWR Year',data=df2,ci=None,legend = 'full',palette = ['red','black'],sort = False)
+axes1 = plt.gca()
+axes1.set_xlabel("Week Number")
+plt.ylabel("Capita per 1000")
+x= df2['MMWR Week']
+plt.xticks(np.arange(min(x), max(x)+1, 1.0))
+plt.xticks(rotation=90)
+"""
+axes2 = axes1.twiny()
+axes2.set_xlabel("Month")
+ticks = np.arange(0,13,1)
+axes2.set_xticks(ticks)
+"""
+
+
+
+
+plt.grid(linewidth = 0.5)
 st.pyplot()
 
 

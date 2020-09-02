0af8b638134894bce94caa9a6027c244cb7b84bb
Mahin <mahin.ramzani@gmail.com>
2020-09-02 18:23:24 -0500

    update setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,7 @@
 mkdir -p ~/.streamlit/
-echo "\
-[general]\n\
-email = \"your@domain.com\"\n\
-" > ~/.streamlit/credentials.toml
-echo "\
+echo “\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+“ > ~/.streamlit/config.toml
